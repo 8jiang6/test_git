366fd33a531c36b422499ea6cdd95de2cec24ce3
jiang <jj2585276461@gmail.com>
2023-03-13 21:18:46 +0800

    scripts -two

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,9 @@ from src.fuctions2 import print_hi
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("method 1:")
     fuctions.print_hi('PyCharm')
+    print('method 2:')
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
