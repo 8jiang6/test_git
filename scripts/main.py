8ae57c243c94d5344e7a817e6a6513a6427ea1d6
jiang <jj2585276461@gmail.com>
2023-03-13 21:17:14 +0800

    scripts -one

---
+++ scripts/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from src import fuctions
+from src.fuctions2 import print_hi
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fuctions.print_hi('PyCharm')
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
